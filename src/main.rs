61a27cb86369482f78ec2af851171f6a42a66534
marior765 <d.bohatyrchk@gmail.com>
2019-04-16 22:55:44 +0300

    impl push

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,38 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 #[warn(dead_code)]
 const STACK_MAX_SIZE: usize = 526;
 const IGCT: i32 = 8;
+
+type SingleLink = Option<Rc<RefCell<s_object>>>;
+
+type StackLink = Rc<RefCell<Vec<SingleLink>>>;
 
 enum ObjectType {
     INT,
     TWIN
 }
 
+// impl std::cmp::PartialEq for ObjectType {
+//     fn eq(&self, other: &ObjectType) -> bool {
+//         self.INT == other.INT
+//     }
+// }
+
 struct s_object {
     type_obj: ObjectType,
     marked: u8,
-    next: Box<s_object>,
-    // unsafe {
-    //     let value: i32;
-    //     struct {
-    //         let head: *sObject;
-    //         let tail: *sObject;
-    //     }
-    // }
+    next: SingleLink,
+    value: i32,
+    head: SingleLink,
+    tail: SingleLink,
 }
 
 struct VM {
-    stack: [Option<s_object>; STACK_MAX_SIZE],
-    stack_size: i32,
-    first_object: Option<s_object>,
+    stack: Vec<SingleLink>,
+    stack_size: usize,
+    first_object: SingleLink,
     num_objects: i32,
     max_objects: i32,
 }
@@ -32,11 +41,45 @@ impl VM {
 
     pub fn init() -> Self {
         VM {
-            stack: [None, STACK_MAX_SIZE],
+            stack: Vec::new(),
             stack_size: 0,
             first_object: None,
             num_objects: 0,
             max_objects: IGCT,
+        }
+    }
+
+    // pub fn pop(&mut self) -> SingleLink {
+    //     self.stack[0]
+    // }
+
+    pub fn push(&mut self, item: s_object) {
+        self.stack_size += 1;
+        self.stack.push(
+            Some(Rc::new(
+                RefCell::new(item)
+            )
+        ))
+    }
+
+    pub fn mark<'a>(&mut self, item: &'a mut s_object) {
+        if item.marked != 1 {
+            item.marked = 1;
+        }
+
+        if item.type_obj == ObjectType::TWIN {
+            match item.head {
+                Some(ref h) => {
+                    self.mark(&mut h.borrow_mut())
+                }
+                None => println!("Head not found!")
+            }
+            match item.tail {
+                Some(ref t) => {
+                    self.mark(&mut t.borrow_mut())
+                }
+                None => println!("Tail not found!")
+            }
         }
     }
 }
