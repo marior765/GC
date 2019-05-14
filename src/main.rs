9fffe66fb340c3eebc081c15c7db265f35e3a691
marior765 <d.bohatyrchk@gmail.com>
2019-05-14 21:25:46 +0300

    init tests

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use std::cell::RefCell;
-use std::rc::Rc;
+use  {
+    std::cell::RefCell,
+    std::rc::Rc,
+    std::option::Option,
+    std::fmt::{
+        Formatter,
+        Display,
+        Result
+    }
+};
 
-#[warn(dead_code)]
 const STACK_MAX_SIZE: usize = 526;
 const IGCT: i32 = 8;
 
-type SingleLink = Option<Rc<RefCell<s_object>>>;
+type SingleLink = Option<Rc<RefCell<Object>>>;
 
 type StackLink = Rc<RefCell<Vec<SingleLink>>>;
 
@@ -15,14 +22,30 @@ enum ObjectType {
     TWIN
 }
 
+impl Display for ObjectType {
+
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{}", self)
+    }
+}
+
 // impl std::cmp::PartialEq for ObjectType {
 //     fn eq(&self, other: &ObjectType) -> bool {
 //         self.INT == other.INT
 //     }
 // }
 
+// fn parse<T>(obj: Option<Rc<RefCell<T>>>) -> Option<T> {
+//     match obj {
+//         Some(o) => {
+//             Some(o.borrow().clone())
+//         }
+//         _ => None
+//     }
+// }
+
 #[derive(Clone)]
-struct s_object {
+struct Object {
     type_obj: ObjectType,
     marked: u8,
     next: SingleLink,
@@ -31,10 +54,10 @@ struct s_object {
     tail: SingleLink,
 }
 
-impl s_object {
+impl Object {
 
     pub fn new(type_obj: ObjectType, marked: u8, next: SingleLink, value: i32, head: SingleLink, tail: SingleLink) -> Self {
-        s_object {
+        Object {
             type_obj,
             marked,
             next,
@@ -69,7 +92,7 @@ impl VM {
     //     self.stack[0]
     // }
 
-    pub fn push(&mut self, item: s_object) {
+    pub fn push(&mut self, item: Object) {
         self.stack_size += 1;
         self.stack.push(
             Some(Rc::new(
@@ -78,7 +101,7 @@ impl VM {
         ))
     }
 
-    pub fn mark<'a>(&mut self, item: &'a mut s_object) {
+    pub fn mark<'a>(&mut self, item: &'a mut Object) {
         if item.marked != 1 {
             item.marked = 1;
         }
@@ -100,14 +123,14 @@ impl VM {
 
     }
 
-    pub fn mark_all(&mut self) {
-        for i in 0..self.stack_size {
-            // self.mark(self.stack[i]);
-            if let Some(ref stack_item) = self.stack[i] {
-                self.mark(&mut stack_item.borrow_mut());
-            }
-        }
-    }
+    // pub fn mark_all(&mut self) {
+    //     for i in 0..self.stack_size {
+    //         // self.mark(self.stack[i]);
+    //         if let Some(ref stack_item) = self.stack[i] {
+    //             self.mark(&mut stack_item.borrow_mut());
+    //         }
+    //     }
+    // }
 
     pub fn markspeep(&mut self) {
         
@@ -116,7 +139,7 @@ impl VM {
     pub fn gc(&mut self) {
         let past_num_objects: i32 = self.num_objects;
 
-        self.mark_all();
+        // self.mark_all();
         self.markspeep();
 
         self.max_objects = self.num_objects * 2;
@@ -124,17 +147,17 @@ impl VM {
         println!("Collected {} objects, {} left", past_num_objects - self.num_objects, self.num_objects );
     }
 
-    pub fn new_object(&mut self, type_obj: ObjectType) -> s_object {
+    pub fn new_object(&mut self, type_obj: ObjectType) -> Object {
         if self.num_objects == self.max_objects { self.gc(); }
 
-        let object: s_object = s_object::new(type_obj, 0, &mut self.first_object.cloned(), 0, None, None);
+        let object: Object = Object::new(type_obj, 0, self.first_object.clone(), 0, None, None);
 
         self.num_objects += 1;
 
         object
     }
 
-    pub fn push_int(self, int_value: i32) {
+    pub fn push_int(&mut self, int_value: i32) {
         let mut object = self.new_object(ObjectType::INT);
 
         object.value = int_value;
@@ -142,32 +165,90 @@ impl VM {
         self.push(object);
     }
 
-    pub fn push_pair(self) -> s_object {
-        let mut object = self.new_object(ObjectType::TWIN);
+    pub fn push_pair(&mut self) {
+        let object = self.new_object(ObjectType::TWIN);
 
         // object.tail = self.pop();
         // object.head = self.pop();
 
         self.push(object);
-        object
+        // object.
     }
 
-    pub fn object_print(self, object: s_object) {
-        match object.type_obj {
-            ObjectType::INT => println!("{}", object.type_obj),
-            ObjectType::TWIN => {
-                print!("(");
-                self.object_print(object.head);
-                print!(", ");
-                self.object_print(object.tail);
-                print!(")");
-            }
-        }
-    }
+    // pub fn object_print(&self, object: Option<Object>) {
+    //     if let Some(o) = object {
+    //         match o.type_obj {
+    //             ObjectType::INT => println!("{}", o.type_obj),
+    //             ObjectType::TWIN => {
+    //                 print!("(");
+    //                 self.object_print(parse(o.head));
+    //                 print!(", ");
+    //                 self.object_print(parse(o.tail));
+    //                 print!(")");
+    //             }
+    //         }
+    //     }
+    // }
 
     pub fn free(mut self) {
         self.stack_size = 0;
         self.gc();
         std::mem::forget(self);
     }
+}
+
+fn first_test() {
+    println!("1: Objects on the stack are preserved.");
+	let mut vm: VM = VM::init();
+	vm.push_int(1);
+	vm.push_int(2);
+	vm.gc();
+	vm.free();
+}
+
+fn second_test() {
+    println!("2: Unreached objects are collected.");
+	let mut vm: VM = VM::init();
+	vm.push_int(1);
+	vm.push_int(2);
+    // vm.pop();
+    // vm.pop();
+	vm.gc();
+	vm.free();
+}
+
+fn third_test() {
+	println!("3: Reach the nested objects.");
+	let mut vm: VM = VM::init();
+	vm.push_int(1);
+	vm.push_int(2);
+	vm.push_pair();
+	vm.push_int(3);
+	vm.push_int(4);
+	vm.push_int(5);
+	vm.push_int(6);
+
+	vm.gc();
+	vm.free();
+}
+
+fn perfomance() {
+    println!("Performance of GC.");
+    let mut vm: VM = VM::init();
+    for i in 0..1000 {
+        for j in 0..20 {
+            vm.push_int(i);
+        }
+        for k in 0..20 {
+            // vm.pop();
+        }
+    }
+    vm.free();
+}
+
+fn main() {
+    first_test();
+    second_test();
+    third_test();
+    perfomance();
 }
